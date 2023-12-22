c9bbc03320c08f7cc0726477e57d6a03c2d1c847
ademkoca <aademkocaa@gmail.com>
2023-12-22 01:36:07 +0100

    add react query

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,23 +2,27 @@ import { Container, ThemeProvider, createTheme } from '@mui/material';
 import { ReactNode } from 'react';
 import Navbar from './navbar';
 import Footer from './footer';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const defaultTheme = createTheme();
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="header" maxWidth="xl" sx={{ paddingX: 0 }}>
-        <Navbar />
-      </Container>
-      {children}
-      <Container
-        component="footer"
-        maxWidth="xl"
-        sx={{ marginBottom: 2, marginTop: 5 }}
-      >
-        <Footer />
-      </Container>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="header" maxWidth="xl" sx={{ paddingX: 0 }}>
+          <Navbar />
+        </Container>
+        {children}
+        <Container
+          component="footer"
+          maxWidth="xl"
+          sx={{ marginBottom: 2, marginTop: 5 }}
+        >
+          <Footer />
+        </Container>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
--- src/pages/progress/index.tsx
+++ src/pages/progress/index.tsx
@@ -3,10 +3,18 @@ import React, { useEffect } from 'react';
 import useGermanStore from '../../store';
 import { getFirstLetterCapitalized } from '../../utils/helpers';
 import { auth } from '../../utils/firebase';
+import { useQuery } from 'react-query';
+import { Progress as ProgressType } from '../../types/interfaces';
 
 const Progress = () => {
   const store = useGermanStore();
   const apiUrl = import.meta.env.VITE_API_URL;
+  const getUser = async () => {
+    const response = await fetch(`${apiUrl}/users/${store.user?._id}`);
+    const res = await response.json();
+    console.log(res.data);
+    return res;
+  };
   const resetProgress = (name: string) => {
     const updatedProgress = store.user?.progress.map((item) => {
       if (item.name === name) {
@@ -36,12 +44,22 @@ const Progress = () => {
       console.log(error);
     }
   };
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['user'],
+    queryFn: getUser,
+    // enabled: !!id,
+  });
+  console.log(data);
+
   useEffect(() => {
     if (!store.user) window.location.href = '/';
   }, [store]);
   useEffect(() => {
     updateUser();
   }, [store.user?.progress]);
+  useEffect(() => {
+    // getUser();
+  }, []);
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -75,7 +93,7 @@ const Progress = () => {
       </Box>
 
       <Box sx={{ minHeight: '80vh' }}>
-        {store.user?.progress.map((p) => (
+        {data?.progress.map((p: ProgressType) => (
           <Box
             key={p.name}
             marginY={2}
